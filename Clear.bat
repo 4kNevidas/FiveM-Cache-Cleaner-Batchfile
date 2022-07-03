23c9a1c27faf2089ea60fb0ba24ed57e94a2253f
Kamionn <88730423+Kamionn@users.noreply.github.com>
2022-07-03 11:56:42 +0200

    Update Clear.bat

--- Clear.bat
+++ Clear.bat
@@ -9,7 +9,6 @@ del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\nui-storage
 del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache
 del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv
 
-
 del /s /f /q C:\Windows\Prefetch\*.*
 del /s /f /q C:\Windows\Temp\*.*
 del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
