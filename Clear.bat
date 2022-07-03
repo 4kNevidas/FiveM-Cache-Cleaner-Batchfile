775af6a27895784571cbcf5037be15043cc3a858
Kamionn <88730423+Kamionn@users.noreply.github.com>
2022-07-03 11:54:21 +0200

    Add files via upload

---
+++ Clear.bat
@@ -0,0 +1,17 @@
+[Clear Cache V2 by Kamion]
+
+@ECHO OFF  & color 0f & cls & title clear.bat 
+
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\logs
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\crashes
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\cache
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\nui-storage
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache
+del /s /f /q %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv
+
+
+del /s /f /q C:\Windows\Prefetch\*.*
+del /s /f /q C:\Windows\Temp\*.*
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+
+[Clear Cache V2 by Kamion]
